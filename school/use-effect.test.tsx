57e89051fc88f39c2597113fbace2f8455f2e2e2
ericminio <eric.mignot@gmail.com>
2021-09-28 14:33:23 -0700

    resists promise

--- school/use-effect.test.tsx
+++ school/use-effect.test.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 describe('useEffect', ()=>{
@@ -66,5 +66,28 @@ describe('useEffect', ()=>{
 
     userEvent.click(screen.getByText('change'));
     expect(screen.getByText(/hi world/)).toBeInTheDocument();
+  });
+
+  it('resists promise', async ()=>{
+    let returningWhat = ()=> Promise.resolve('hello world');
+
+    let Hello = ({getData})=> {
+      const [message, setMessage] = useState();
+
+      useEffect(()=>{
+        getData()
+          .then(value => setMessage(value))
+          .catch(error => setMessage(null, error.message));
+      })
+      
+      return (
+        <>
+          <div>${message}</div>
+        </>
+      )
+    }
+    render(<Hello getData={returningWhat}/>);
+
+    await waitFor(() => screen.getByText(/hello world/));
   });
 });
