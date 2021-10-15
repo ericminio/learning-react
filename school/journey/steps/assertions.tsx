e74bfffaa4ed70934c61b15b1fee10ce64610d2d
ericminio <eric.mignot@gmail.com>
2021-10-14 21:22:43 -0700

    getByRole

--- school/journey/steps/assertions.tsx
+++ school/journey/steps/assertions.tsx
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
 
 export function thenWeSeeTheFirstPage() {
-    expect(screen.getByText(/go/)).toBeInTheDocument();
+    expect(screen.getByRole('button', { name:'go' })).toBeInTheDocument();
 }
 export function thenWeSeeTheSecondPage() {
     expect(screen.getByText(/landed/)).toBeInTheDocument();
