9e13edd444dbe0d2a911a4adeabb3a09582c763d
ericminio <eric.mignot@gmail.com>
2022-02-02 16:55:22 -0800

    about async redirect

--- school/redirect.test.tsx
+++ school/redirect.test.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MemoryRouter, Switch, Route, Redirect } from "react-router-dom";
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 describe('Redirect', ()=>{
 
@@ -21,4 +21,44 @@ describe('Redirect', ()=>{
 
     expect(screen.getByText(/landed/)).toBeInTheDocument();
   });
+
+  it('can happen async', async ()=>{
+    let Initial = ()=> {
+        const [shouldRedirect, setShouldRedirect] = useState(false);
+        useEffect(() => {
+            setTimeout(() => {
+                setShouldRedirect(true)
+            }, 150)
+        })
+        return (
+            <>
+                { shouldRedirect && <Redirect to="/final" /> }
+                <div>initial</div>
+            </>
+        )
+    }
+    let Final = ()=> {
+        return (
+            <div>landed</div>
+        )
+    }
+    render(
+        <MemoryRouter initialEntries={["/initial"]}>
+            <Switch>
+                <Route path="/initial">
+                    <Initial />
+                </Route>
+                <Route path="/final">
+                    <Final />
+                </Route>
+            </Switch>
+        </MemoryRouter>
+    );
+    expect(screen.getByText(/initial/)).toBeInTheDocument();
+    await waitFor(() => {
+        expect(screen.getByText(/landed/)).toBeInTheDocument();
+    });    
+  });
 });
+
+
