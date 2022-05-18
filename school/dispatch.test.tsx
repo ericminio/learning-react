7af54ea7c1e4cb71a426be1c425c6558b2d472a3
ericminio <eric.mignot@gmail.com>
2022-05-18 07:09:07 -0700

    about dispatch

---
+++ school/dispatch.test.tsx
@@ -0,0 +1,54 @@
+import React, { useReducer, useContext, useMemo } from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
+
+describe('dispatch', ()=>{
+
+  it('can be shared to simulate eda', ()=>{
+    const CustomContext = React.createContext(undefined);
+    function CustomContextProvider({children}) {
+      const initial = { message:'hello world' };
+      const reducer = (state)=> {
+        return { message:'hi world' };
+      }
+      const [state, dispatch] = useReducer(reducer, initial);
+      const contextValue = useMemo(() => ({ state, dispatch}), [state, dispatch])
+      return (
+        <>
+          <CustomContext.Provider value={contextValue}>      
+            {children}      
+          </CustomContext.Provider>
+        </>
+      )
+    }
+
+    function Source() {      
+      const { state, dispatch } = useContext(CustomContext);
+
+      return (
+        <div>
+          <button onClick={ ()=> { dispatch(); }}>change</button>
+        </div>
+      )
+    }
+    function Target() {
+      const { state } = useContext(CustomContext);
+
+      return (
+        <div>
+          <div>${state.message}</div>
+        </div>
+      )
+    }
+    render(<>
+      <CustomContextProvider>
+        <Source /> 
+        <Target />
+      </CustomContextProvider>
+    </>);
+    userEvent.click(screen.getByText('change'));
+
+    expect(screen.getByText(/hi world/)).toBeInTheDocument();
+  });
+});
