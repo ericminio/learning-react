d07c0ccd31eb7c3dda248065ace50c181df7bb16
ericminio <eric.mignot@gmail.com>
2022-05-20 07:03:41 -0700

    more about dispatch

--- school/dispatch.test.tsx
+++ school/dispatch.test.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useContext, useMemo } from 'react';
+import React, { useReducer, useContext, useMemo, useEffect } from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event';
@@ -48,6 +48,52 @@ describe('dispatch', ()=>{
       </CustomContextProvider>
     </>);
     userEvent.click(screen.getByText('change'));
+
+    expect(screen.getByText(/hi world/)).toBeInTheDocument();
+  });
+
+  it('can be called from test', ()=>{
+    const CustomContext = React.createContext(undefined);
+    function CustomContextProvider({children}) {
+      const initial = { message:'hello world' };
+      const reducer = (state)=> {
+        return { message:'hi world' };
+      }
+      const [state, dispatch] = useReducer(reducer, initial);
+      const contextValue = useMemo(() => ({ state, dispatch}), [state, dispatch])
+      return (
+        <>
+          <CustomContext.Provider value={contextValue}>      
+            {children}      
+          </CustomContext.Provider>
+        </>
+      )
+    }
+
+    function Source({children}: React.PropsWithChildren) {
+      const { dispatch } = useContext(CustomContext);
+      
+      useEffect(() => { dispatch() }, [dispatch]);
+
+      return (<>{children}</>)
+    }
+    function Target() {
+      const { state } = useContext(CustomContext);
+
+      return (
+        <div>
+          <div>${state.message}</div>
+        </div>
+      )
+    }
+    render(<>
+      <CustomContextProvider>
+        <Source>
+          <Target />
+        </Source>
+      </CustomContextProvider>
+    </>);
+    
 
     expect(screen.getByText(/hi world/)).toBeInTheDocument();
   });
