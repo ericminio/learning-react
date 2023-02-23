80c70e63e201910ba789e5bc746cbebe98723a12
ericminio <emignot@change.org>
2023-02-23 15:15:55 -0800

    extract getTextContent

--- school/text-content.test.tsx
+++ school/text-content.test.tsx
@@ -1,24 +1,24 @@
 import * as React from 'react';
 import '@testing-library/jest-dom';
 
-describe('textContent', () => {
-    it('is not native', () => {
-        const getNodeText = (node) => {
-            if (['string', 'number'].includes(typeof node)) return node;
-            if (node instanceof Array) return node.map(getNodeText).join('');
-            if (
-                typeof node === 'object' &&
-                node &&
-                node.type &&
-                ['function'].includes(typeof node.type)
-            ) {
-                return getNodeText(node.type());
-            }
-            if (typeof node === 'object' && node) {
-                return getNodeText(node.props.children);
-            }
-        };
+const getTextContent = (node) => {
+    if (['string', 'number'].includes(typeof node)) return node;
+    if (node instanceof Array) return node.map(getTextContent).join('');
+    if (
+        typeof node === 'object' &&
+        node &&
+        node.type &&
+        ['function'].includes(typeof node.type)
+    ) {
+        return getTextContent(node.type());
+    }
+    if (typeof node === 'object' && node) {
+        return getTextContent(node.props.children);
+    }
+};
 
+describe('textContent util', () => {
+    it('is not native', () => {
         const Hello = (): JSX.Element => {
             return <div>hello world</div>;
         };
@@ -29,6 +29,6 @@ describe('textContent', () => {
             </div>
         );
 
-        expect(getNodeText(wrapper)).toEqual('wrapping:hello world');
+        expect(getTextContent(wrapper)).toEqual('wrapping:hello world');
     });
 });
