e7819f32dacf0592a174d3843485fa117345cc29
ericminio <emignot@change.org>
2023-02-23 11:59:53 -0800

    about nested components

--- school/jest.test.tsx
+++ school/jest.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
-describe('Jest', () => {
+describe.only('Jest', () => {
     it('is ready', () => {
         render(<div data-testid="this-id">hello world</div>);
 
@@ -16,5 +16,23 @@ describe('Jest', () => {
         render(<Hello />);
 
         expect(screen.getByTestId('this-id')).toHaveTextContent('hello');
+    });
+
+    it('welcomes nested component', () => {
+        function Hello() {
+            return <div data-testid="this-id">hello world</div>;
+        }
+        function Wrapper() {
+            return (
+                <div data-testid="this-wrapper">
+                    wrapping <Hello />
+                </div>
+            );
+        }
+        render(<Wrapper />);
+
+        expect(screen.getByTestId('this-wrapper')).toHaveTextContent(
+            'wrapping hello world'
+        );
     });
 });
