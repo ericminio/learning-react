ac302a5bf1ec52a8d138e7b5d127a1b9293741a8
ericminio <eric.mignot@gmail.com>
2024-06-25 14:20:23 -0700

    use webapp.js as bundle name

--- school/jsdom/app/server/app.js
+++ school/jsdom/app/server/app.js
@@ -13,9 +13,9 @@ app.get('/', function (_, response) {
     response.end();
 });
 
-app.get('/app.js', function (_, response) {
+app.get('/webapp.js', function (_, response) {
     response.setHeader('Content-Type', 'text/javascript');
-    const code = fs.readFileSync('school/jsdom/build/app.js').toString();
+    const code = fs.readFileSync('school/jsdom/build/webapp.js').toString();
 
     response.write(code);
     response.end();
