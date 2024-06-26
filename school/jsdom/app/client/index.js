283fb9d838ef3e5fe83d700c5186c02dd6319f2d
Eric Mignot <eric.mignot@gmail.com>
2024-06-26 14:45:19 -0700

    typescript

--- school/jsdom/app/client/index.js
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-
-const container = document.getElementById('root');
-ReactDOM.createRoot(container).render(<App />);
--- school/jsdom/build/webapp.js
+++ school/jsdom/build/webapp.js
@@ -1,32 +1,47 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.App = App;
-var _react = require("react");
-var _jsxRuntime = require("react/jsx-runtime");
+var _react = _interopRequireWildcard(require("react"));
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
 function App() {
-  const [data, setData] = (0, _react.useState)([]);
-  (0, _react.useEffect)(() => {
-    fetch('/data').then(response => response.json()).then(incoming => {
+  var _useState = (0, _react.useState)(),
+    _useState2 = _slicedToArray(_useState, 2),
+    data = _useState2[0],
+    setData = _useState2[1];
+  (0, _react.useEffect)(function () {
+    fetch('/data').then(function (response) {
+      return response.json();
+    }).then(function (incoming) {
       setData(incoming);
     });
   }, []);
-  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
-    children: ["Received: ", data.message]
-  });
+  return /*#__PURE__*/_react["default"].createElement("div", null, "Received: ", data === null || data === void 0 ? void 0 : data.message);
 }
 
-},{"react":"react","react/jsx-runtime":"react/jsx-runtime"}],2:[function(require,module,exports){
+},{"react":"react"}],2:[function(require,module,exports){
 "use strict";
 
+var _react = _interopRequireDefault(require("react"));
 var _client = _interopRequireDefault(require("react-dom/client"));
 var _App = require("./App");
-var _jsxRuntime = require("react/jsx-runtime");
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
-const container = document.getElementById('root');
-_client.default.createRoot(container).render( /*#__PURE__*/(0, _jsxRuntime.jsx)(_App.App, {}));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var container = document.getElementById('root');
+if (!container) {
+  throw new Error('root element missing in html');
+}
+var root = _client["default"].createRoot(container);
+root.render( /*#__PURE__*/_react["default"].createElement(_App.App, null));
 
-},{"./App":1,"react-dom/client":"react-dom/client","react/jsx-runtime":"react/jsx-runtime"}]},{},[2]);
+},{"./App":1,"react":"react","react-dom/client":"react-dom/client"}]},{},[2]);
